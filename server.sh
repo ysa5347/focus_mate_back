7816fdc4aa9388a1e9ef335d9ff40a5bf68e65c9
ysa5347 <ysa5347@gmail.com>
2024-01-01 18:52:42 +0900

    TEST; mysql connection error bugfix(4)

--- server.sh
+++ server.sh
@@ -2,7 +2,6 @@
 cd /with_ance/with_ance_app/
 cat .env
 cd ./with_ance/
-
 python3 manage.py makemigrations
 python3 manage.py migrate
-python3 manage.py runserver 0.0.0.0:80
+python3 manage.py runserver 0.0.0.0:80  
