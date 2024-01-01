5ced2e62b37cc3de2e276da09ab444868c78c240
ysa5347 <ysa5347@gmail.com>
2024-01-01 13:44:32 +0900

    TEST; env test

--- server.sh
+++ server.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 ls
-cd /with_ance/with_ance_app/with_ance/
+cd /with_ance/with_ance_app/
+cat .env
+cd /with_ance/
+
 python3 manage.py makemigrations
 python3 manage.py migrate
 python3 manage.py runserver 0.0.0.0:80
