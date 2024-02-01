643366d873010f58a3d6ff0c0e3e52bfe9bc5f90
ysa5347 <ysa5347@gmail.com>
2024-02-01 10:51:48 +0900

    FIX; secret update(1)

--- with_ance/with_ance/settings.py
+++ with_ance/with_ance/settings.py
@@ -23,7 +23,7 @@ load_dotenv()
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
+SECRET_KEY = os.environ.get('DJANGO_SECRETKEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
